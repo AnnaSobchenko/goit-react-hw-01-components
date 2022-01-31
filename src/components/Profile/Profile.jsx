22e42cbf6beeb41256d199d830748d6e4d589cf8
AnnaSobchenko <annsbchnk@gmail.com>
2022-01-31 18:22:02 +0200

    task1

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,17 @@
+import user from "./user.json";
+import UserCard from "./UserProfile";
+
+ const Profile=()=>{
+return (
+  <>
+  <UserCard
+        key={user.id}
+        userName={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}  
+      />
+  </>
+);}
+export default Profile;
