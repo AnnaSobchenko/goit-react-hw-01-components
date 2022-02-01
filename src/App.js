3ca3d02536e2bbf3cbe611422a79e1eb835cfa25
AnnaSobchenko <annsbchnk@gmail.com>
2022-02-01 18:00:11 +0200

    task1_scss

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import "./App.css";
 import FriendList from "./components/FriendList/FriendList";
 import UserProfile from "./components/Profile/Profile";
-import Statistics from "./components/Statistics/statistics";
+import { ContainerStyled } from "./components/shared/Container.styled";
+import Statistics from "./components/Statistics/Statistics";
 import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
 
 function App() {
   return (
-    <>
+    <ContainerStyled>
       <UserProfile />
       <Statistics />
       <FriendList />
       <TransactionHistory />
-    </>
+    </ContainerStyled>
   );
 }
 
---
+++ src/components/shared/Container.styled.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const ContainerStyled = styled.div`
+  /* width: 1000px; */
+  padding: 60px 15px;
+  background-color:#ccc;
+`;
